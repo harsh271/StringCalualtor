d0546b44cf59826c61245b5ecdfb5b9ea640f6a3
jalharsh <harshsharma271@gmail.com>
2020-05-26 17:31:43 +0530

    test Case added for ReturningSum on MultipleNumbers,succeed Scenerio

--- src/main/java/com/example/demo/StringCalculator.java
+++ src/main/java/com/example/demo/StringCalculator.java
@@ -1,19 +1,21 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class StringCalculator {
-
+	static Logger logger = LoggerFactory.getLogger(StringCalculator.class);
 	public static int addString(String text) {
-		if(text.isEmpty()) {
-		return 0;
-		}else if(text.contains(",")) {
-			String[] tokens =text.split(",");
-			return toInt(tokens[0])+toInt(tokens[1]);
-		}else {
-			return toInt(text);
+		int sum = 0;
+		try {
+			String[] tokens=text.split(",");
+			for (String token : tokens) {
+				int i=Integer.parseInt(token);
+				sum=sum+i;
+			}
+		} catch (Exception e) {
+			logger.warn("Invalid Input");
 		}
-	}
-
-	private static int toInt(String text) {
-		return Integer.parseInt(text);
-	}
+		return sum;
+}
 }
