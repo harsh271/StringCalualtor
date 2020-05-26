b7027bcd178d51ba2186352d0ae942f330d42139
jalharsh <harshsharma271@gmail.com>
2020-05-26 16:54:56 +0530

    Should Return Number on Number Test Succeed

--- src/main/java/com/example/demo/StringCalculator.java
+++ src/main/java/com/example/demo/StringCalculator.java
@@ -2,8 +2,11 @@ package com.example.demo;
 
 public class StringCalculator {
 
-	public static Object addString(String string) {
+	public static int addString(String text) {
+		if(text.isEmpty()) {
 		return 0;
+		}else {
+			return Integer.parseInt(text);
+		}
 	}
-
 }
